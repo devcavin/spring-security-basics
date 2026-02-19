5d51d4ab4d655fc592d27aa11b532408f30cb083
Cavin <cavinlarry001@gmail.com>
2026-02-19 21:18:19 +0300

    Initial commit rollback

--- src/main/kotlin/dev/killercavin/springsecuritybasics/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package dev.killercavin.springsecuritybasics
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository: JpaRepository<User, Long> {
-    fun getUserByUsername(username: String): User?
-    fun existsUserByUsername(username: String): Boolean
-}
